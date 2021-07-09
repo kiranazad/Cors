2c3ff14a4a80703e574dd409543d7b6866140e89
kiranb <kiranb@BKIRAN>
2021-07-09 20:36:12 +0530

    Cors Changes

---
+++ CorsDemo/CorsDemo/Controllers/CorsFixController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CorsDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CorsFixController : Controller
+    {
+        //STEP -3 Add CORS only for perticular method in the controller.
+        [Route("loginInWithIlsCredentials")]
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult<LoginResult>> LoginAsync([FromBody] LoginRequest request)
+        {
+            var data = GetAccessToken();
+            return Ok(new LoginResult
+            {
+                AccessToken = data.AccessToken,
+                AccessTokenExpiresIn = data.AccessTokenExpiresIn,
+                RefreshToken = data.RefreshToken
+            });
+        }
+
+        [EnableCors("Policy1")]
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "TESTING";
+        }
+        private static string GetConnectionString()
+        {
+            String connString = "User Id=appuser;Password=hillary;Data Source=(DESCRIPTION=(ADDRESS= (PROTOCOL=TCP) (HOST=172.16.99.64)  (PORT=1521) )(CONNECT_DATA= (SID = TEAMJQA) (server = shared)))";
+            return connString;
+        }
+
+        private static LoginResult GetAccessToken()
+        {
+            string connectionString = GetConnectionString();
+            var loginResult = new LoginResult();
+            using (OracleConnection connection = new OracleConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                OracleCommand command = connection.CreateCommand();
+                string sql = "SELECT * FROM INV.ILSAUTH";
+                command.CommandText = sql;
+
+                OracleDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    loginResult = new LoginResult { 
+                         AccessToken = (string)reader["ACCESSTOKEN"],
+                         AccessTokenExpiresIn = Convert.ToDouble(reader["EXPIRESIN"]),
+                         RefreshToken = (string)reader["REFRESHTOKEN"]
+                    };
+                }
+            }
+            return loginResult;
+        }
+    }
+    
+    public class LoginResult
+    {
+        public string AccessToken { get; set; }
+        /// <summary>
+        /// The number of seconds in which the access token expires
+        /// </summary>
+        public double AccessTokenExpiresIn { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+    
+}
---
+++ CorsDemo/CorsDemo/Controllers/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CorsDemo.Controllers
+{
+    public class LoginRequest
+    {
+            [Required(ErrorMessage = "Username is required.")]
+            public string Username { get; set; }
+
+            [Required(ErrorMessage = "Password is required.")]
+            public string Password { get; set; }
+
+            [Required(ErrorMessage = "TenantId is required.")]
+            [Range(1, long.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}")]
+            public long TenantId { get; set; }
+    }
+}
